36f019d900f9f660234e6be6e529272e39be0252
Brock Belmonte <brockbel@gmail.com>
2018-05-15 19:10:13 -0500

    Relational db's, expiration logic, form input

--- models/grocery.js
+++ models/grocery.js
@@ -2,9 +2,22 @@ module.exports = function(sequelize, DataTypes) {
     var Grocery = sequelize.define("Grocery", {
       foodProduct: DataTypes.STRING,
       quantity: DataTypes.DOUBLE,
-      expirationStatus: DataTypes.INTEGER,
       expirationDate: DataTypes.DATE,
       expirationNotification: DataTypes.DATE
     });
+    Grocery.associate = function(models) {
+      models.Grocery.belongsTo(models.User);
+    };
+    // Grocery.getExpirationStatus will return 0, 1, or 2 and then you can use that to display it in the proper section of the html page.
+    Grocery.prototype.getExpirationStatus = function() {
+      var today = Date.now();
+      if(today >= this.expirationDate) {
+        return 2;
+      }else if(today >= this.expirationNotification) {
+        return 1;
+      }else{
+        return 0;
+      }
+    };
     return Grocery;
   };
