b9e0b8e02ac20af6d4b428dcd7a697d370ead63c
Ozair Khan <ozairsaddress@gmail.com>
2018-05-18 23:59:16 -0600

    expiring and expiring soon categories working

--- public/js/members.js
+++ public/js/members.js
@@ -2,21 +2,31 @@
 $.get("/members/getgroceries2").then(function (data) {
   for (var i = 0; i < data.length; i++) {
     if(!data[i].ownedItem){
-    $("#groceriesToGet").append(`${data[i].foodProduct} <button class="addToPantry" data-id=${data[i].id}> Add To Pantry </button> <br>`);
+      $("#groceriesToGet").append(`${data[i].foodProduct} <button class="addToPantry" data-id=${data[i].id}> Add To Pantry </button> <br>`);
     }
     else{
-      $("#groceriesOwned").append(`${data[i].foodProduct} <br>`);
+      var today = moment().format();
+      if(today >= data[i].expirationDate) {
+        $("#groceriesExpired").append(`${data[i].foodProduct} <br>`);
+      }else if(today >= data[i].expirationNotification) {
+        $("#groceriesExpiringSoon").append(`${data[i].foodProduct} <br>`);
+      }else{
+        $("#groceriesOwned").append(`${data[i].foodProduct} <br>`);
+      }
     }
   }
 });
+
+
+// console.log(moment().format())
 
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.email);
   });
-
-
+  
+  
   // this will update an item to be in the pantry and update DOM
   $(document).on('click', '.addToPantry', function(){ 
     $.ajax("/members/updategroceries", {
@@ -29,15 +39,15 @@ $.get("/members/getgroceries2").then(function (data) {
           $("#groceriesOwned").empty();
           for (var i = 0; i < data.length; i++) {
             if(!data[i].ownedItem){
-            $("#groceriesToGet").append(`${data[i].foodProduct} <button class="addToPantry" data-id=${data[i].id}> Add To Pantry </button> <br>`);
+              $("#groceriesToGet").append(`${data[i].foodProduct} <button class="addToPantry" data-id=${data[i].id}> Add To Pantry </button> <br>`);
             }
             else{
               $("#groceriesOwned").append(`${data[i].foodProduct} <br>`);
             }
           }
         });
-
+        
       });
-  });
-
+    });
+    
 
